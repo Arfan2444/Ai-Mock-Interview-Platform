8d6744a937231ccea714694adb5b5e6034f40f58
Arfan <pathanarfan8@gmail.com>
2026-01-11 17:59:33 +0530

    deployment

--- app/dashboard/_components/InterviewList.jsx
+++ app/dashboard/_components/InterviewList.jsx
@@ -30,7 +30,7 @@ function InterviewList() {
   return (
     <div>
       <h2 className="font-medium text-2xl">Previous Interview List</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {interviewList.map((interview, index) => (
           <InterviewItemCard
             key={interview.id ?? index}
